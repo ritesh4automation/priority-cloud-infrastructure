66b25a02bbf1641cd8b423f8af9dbea25fbfdfe3
Ritesh Pandey <rp@prioritycloud.in>
2025-12-18 13:28:49 +0530

    Add security module and workflow

--- accounts/dev-account-alpha/security/main.tf
+++ accounts/dev-account-alpha/security/main.tf
@@ -1,46 +1,36 @@
 data "terraform_remote_state" "network" {
-backend = "s3"
+  backend = "s3"
 
-
-config = {
-bucket = "prioritycloudinfra-tf-state"
-key = "dev-account-alpha/network/terraform.tfstate"
-region = "ap-south-1"
-}
-}
-
-
-resource "aws_security_group" "web" {
-name = "dev-web-sg"
-vpc_id = data.terraform_remote_state.network.outputs.vpc_id
-
-
-ingress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+  config = {
+    bucket = "prioritycloudinfra-tf-state"
+    key    = "dev-account-alpha/network/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
+module "web_sg" {
+  source = "../../../modules/security_groups"
 
-ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
+  name   = "dev-web-sg"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 
+  ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 
-egress {
-from_port = 0
-to_port = 0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-tags = {
-Environment = "dev"
-Service = "security"
-}
+  tags = {
+    Environment = "dev"
+    Service     = "security"
+  }
 }
