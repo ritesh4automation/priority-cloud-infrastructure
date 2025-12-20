45bd782ef44b381b2972b905c5770f197be942ba
Ritesh Pandey <rp@prioritycloud.in>
2025-12-20 20:10:33 +0530

    feat(network): add third subnet

--- accounts/dev-account-alpha/network/main.tf
+++ accounts/dev-account-alpha/network/main.tf
@@ -5,12 +5,14 @@ module "vpc" {
 
   public_subnets = [
     "10.10.1.0/24",
-    "10.10.2.0/24"
+    "10.10.2.0/24",
+     "10.10.3.0/24" 
   ]
 
   azs = [
     "ap-south-1a",
-    "ap-south-1b"
+    "ap-south-1b",
+    "ap-south-1c"
   ]
 
   tags = {
